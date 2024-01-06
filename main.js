de74f1cd0b4d8b05024ae45fdf576115883866d0
Mike Mantecon <52804390+mikemantecon@users.noreply.github.com>
2024-01-06 15:57:01 -0800

    function structure created to find winner of Rock Paper Scissors
    This function is not yet complete over all conditions, but presumably carries the necessary logic.
    
    It takes the result from the computer generation of rock paper scissors and also prompts the user what their selection will be.
    
    It then compares the results and decides who won.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 function getComputerChoice () {
   // Generates a random number between 1 and 3
-  let numberChosen = Math.floor(Math.random() * 3)
+  let numberChosen = Math.floor(Math.random() * 3);
   // assigns a value to the number
   if (numberChosen = 3) {
     getComputerChoice = "rock"
@@ -11,9 +11,38 @@ function getComputerChoice () {
   } else if (numberChosen = 1) {
     getComputerChoice = "scissors"
   } else {
-    console.log("Debugging Needed")
+    console.log("Debugging Needed 14")
 }
 
 // Initialize a function that plays a single round of Rock Paper Scissors. The parameters should be playerSelection and computerSelection, and at the end it should return a string that declares the winner of the round like so: "You Lose! Paper beats Rock. The playerSelection parameter should be case-insensitive and a tie should cause a replay. 
+
+function rockPaperScissors (playerSelection, computerSelection) {
+  // Ask for the player selection, without being case sensitive
+  let playerSelection = prompt("It's Your Turn Now! Will it be Rock, Paper, or Scissors? ").toLowerCase;
+  // Retrieve Computer selection
+  let computerSelection = getComputerChoice; 
+  // Conditional to assing value to the choice
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie!! Let's go for the tiebreaker...")
+    return rockPaperScissors;
+  } else {
+    if (playerSelection === "rock") {
+      if (computerSelection === "scissors") {
+        console.log("YOU WIN! Rock crushes scissors.");
+      } else if (computerSelection === "paper") {
+        console.log("Uh oh. You Lose. Paper covers rock. :(");
+      } else {
+        console.log("Debugging Needed 35");
+      }
+    } else if (playerSelection === "paper") {
+// Note: Result of condition to be added here
+    } else if (playerSelection === "scissors") {
+// Note: Result of condition to be added here
+    } else {
+      console.log("I didn't seem to catch that Make sure you are giving a rock/paper/scissors value (not case sensitive).");
+      return rockPaperScissors;
+    }
+  }
+}
 
 // Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. Use prompt() to get input from the user. 
