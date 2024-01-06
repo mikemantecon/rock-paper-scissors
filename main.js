ab36552f69519e4a36c6dbaba1f3954573ba4814
Mike Mantecon <52804390+mikemantecon@users.noreply.github.com>
2024-01-06 15:17:37 -0800

    Wrote out a Pseudocode Plan
    In the main.js file, the comment now present a way of going about getting the desired result.

---
+++ main.js
@@ -0,0 +1,5 @@
+// Initialize a function that randomly returns either 'Rock', 'Paper' or 'Scissors'.
+
+// Initialize a function that plays a single round of Rock Paper Scissors. The parameters should be playerSelection and computerSelection, and at the end it should return a string that declares the winner of the round like so: "You Lose! Paper beats Rock. The playerSelection parameter should be case-insensitive and a tie should cause a replay. 
+
+// Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. Use prompt() to get input from the user. 
